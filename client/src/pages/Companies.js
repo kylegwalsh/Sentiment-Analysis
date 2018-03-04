72622b753a048c72941db6cab6c4b2ea2e549e50
trenflem@gmail.com <trenflem@gmail.com>
2018-03-04 07:09:12 -0500

    added sentiment pie

--- client/src/pages/Companies.js
+++ client/src/pages/Companies.js
@@ -6,7 +6,7 @@ import { connect } from 'react-redux';
 class _Companies extends Component {
   constructor(props) {
     super(props);
-    this.state = { companies: [] };
+    this.state = {companies: []};
 
     this.blues = [
       "#A3E2FF",
@@ -24,59 +24,54 @@ class _Companies extends Component {
       .then((res) => res.json())
       .then((resJson) => {
         console.log(resJson)
-        this.setState({ companies: resJson.companies });
+        this.setState({companies: resJson.companies});
       });
   }
 
-  // renderSentimentPie() {
-  //   const today = new Date();
-  //   const month = today.getMonth();
-  //   const {companies} = this.state;
-  //   const {gender, marital} = this.props.filters;
-  //
-  //   const sentimentData = {
-  //     labels: [
-  //       'Positive',
-  //       'Neutral',
-  //       'Negative',
-  //       'Mixed'
-  //     ],
-  //     datasets: [{
-  //       data: [0, 0, 0, 0],
-  //       backgroundColor: [
-  //       '#A3E2FF',
-  //       '#24B8FD',
-  //       '#0072A7',
-  //       '#003a55'
-  //       ],
-  //       hoverBackgroundColor: [
-  //       '#bce7fb',
-  //       '#45b2e6',
-  //       '#0d6b96',
-  //       '#064b6b'
-  //       ]
-  //     }]
-  //   };
-  //
-  //   this.state.companies[this.props.match.params.name].data.forEach((row) => {
-  //     if((month === row.time) && (!gender || gender === row.gender) && (!marital || marital === row.marital)) {
-  //       if(row.ethnicity === "American Indian") ethnicityData.datasets[0] = this.ethnicity.datasets[0] + row.count;
-  //       else if(row.gender === "Asian") this.genderData.datasets[1] = this.genderData.datasets[1] + row.count;
-  //       else if(row.gender === "Black") this.genderData.datasets[2] = this.genderData.datasets[2] + row.count;
-  //       else if(row.gender === "Hispanic") this.genderData.datasets[3] = this.genderData.datasets[3] + row.count;
-  //       else if(row.gender === "Pacific Islander") this.genderData.datasets[4] = this.genderData.datasets[4] + row.count;
-  //       else if(row.gender === "White") this.genderData.datasets[5] = this.genderData.datasets[5] + row.count;
-  //       else this.genderData.datasets[6] = this.genderData.datasets[6] + row.count;
-  //     }
-  //   });
-  //
-  //   return sentimentData;
-  // }
+  getSentimentPieData() {
+    const today = new Date();
+    const month = today.getMonth();
+    const {companies} = this.state;
+    const {gender, marital} = this.props.filters;
+
+    const sentimentData = {
+      labels: [
+        'Positive',
+        'Neutral',
+        'Negative',
+        'Mixed'
+      ],
+      datasets: [{
+        data: [0, 0, 0, 0],
+        backgroundColor: [
+          '#A3E2FF',
+          '#24B8FD',
+          '#0072A7',
+          '#003a55'
+        ],
+        hoverBackgroundColor: [
+          '#bce7fb',
+          '#45b2e6',
+          '#0d6b96',
+          '#064b6b'
+        ]
+      }]
+    };
+
+    const {positive, neutral, negative, mixed, totalCount} = this.getDifferentSentiments();
+    sentimentData.datasets[0].data = [
+      positive / totalCount,
+      neutral / totalCount,
+      negative / totalCount,
+      mixed / totalCount,
+    ];
+    return sentimentData;
+  }
 
   getGenderPieData = () => {
     const today = new Date();
     const month = today.getMonth();
-    const { companies } = this.state;
+    const {companies} = this.state;
     const {ethnicity, marital} = this.props.filters;
 
     const genderData = {
@@ -88,22 +83,22 @@ class _Companies extends Component {
       datasets: [{
         data: [0, 0, 0],
         backgroundColor: [
-        '#5BCBFF',
-        '#0092D6',
-        '#004F74'
+          '#5BCBFF',
+          '#0092D6',
+          '#004F74'
         ],
         hoverBackgroundColor: [
-        '#7ccff6',
-        '#28a4de',
-        '#045c85'
+          '#7ccff6',
+          '#28a4de',
+          '#045c85'
         ]
       }]
     };
 
     this.state.companies[this.props.match.params.name].data.forEach((row) => {
-      if((month === parseInt(row.time)) && (!ethnicity || ethnicity === row.race.trim()) && (!marital || marital === row.marital.trim())) {
-        if(row.gender.trim() === "Male") genderData.datasets[0].data[0] = genderData.datasets[0].data[0] + parseInt(row.count);
-        else if(row.gender.trim() === "Female") genderData.datasets[0].data[1] = genderData.datasets[0].data[1] + parseInt(row.count);
+      if ((month === parseInt(row.time)) && (!ethnicity || ethnicity === row.race.trim()) && (!marital || marital === row.marital.trim())) {
+        if (row.gender.trim() === "Male") genderData.datasets[0].data[0] = genderData.datasets[0].data[0] + parseInt(row.count);
+        else if (row.gender.trim() === "Female") genderData.datasets[0].data[1] = genderData.datasets[0].data[1] + parseInt(row.count);
         else genderData.datasets[0].data[2] = genderData.datasets[0].data[2] + parseInt(row.count);
       }
     });
@@ -151,13 +146,13 @@ class _Companies extends Component {
     };
 
     this.state.companies[this.props.match.params.name].data.forEach((row) => {
-      if((month === parseInt(row.time)) && (!gender || gender === row.gender.trim()) && (!marital || marital === row.marital.trim())) {
-        if(row.race.trim() === "American Indian") ethnicityData.datasets[0].data[0] = ethnicityData.datasets[0].data[0] + parseInt(row.count);
-        else if(row.race.trim() === "Asian") ethnicityData.datasets[0].data[1] = ethnicityData.datasets[0].data[1] + parseInt(row.count);
-        else if(row.race.trim() === "Black") ethnicityData.datasets[0].data[2] = ethnicityData.datasets[0].data[2] + parseInt(row.count);
-        else if(row.race.trim() === "Hispanic") ethnicityData.datasets[0].data[3] = ethnicityData.datasets[0].data[3] + parseInt(row.count);
-        else if(row.race.trim() === "Pacific Islander") ethnicityData.datasets[0].data[4] = ethnicityData.datasets[0].data[4] + parseInt(row.count);
-        else if(row.race.trim() === "White") ethnicityData.datasets[0].data[5] = ethnicityData.datasets[0].data[5] + parseInt(row.count);
+      if ((month === parseInt(row.time)) && (!gender || gender === row.gender.trim()) && (!marital || marital === row.marital.trim())) {
+        if (row.race.trim() === "American Indian") ethnicityData.datasets[0].data[0] = ethnicityData.datasets[0].data[0] + parseInt(row.count);
+        else if (row.race.trim() === "Asian") ethnicityData.datasets[0].data[1] = ethnicityData.datasets[0].data[1] + parseInt(row.count);
+        else if (row.race.trim() === "Black") ethnicityData.datasets[0].data[2] = ethnicityData.datasets[0].data[2] + parseInt(row.count);
+        else if (row.race.trim() === "Hispanic") ethnicityData.datasets[0].data[3] = ethnicityData.datasets[0].data[3] + parseInt(row.count);
+        else if (row.race.trim() === "Pacific Islander") ethnicityData.datasets[0].data[4] = ethnicityData.datasets[0].data[4] + parseInt(row.count);
+        else if (row.race.trim() === "White") ethnicityData.datasets[0].data[5] = ethnicityData.datasets[0].data[5] + parseInt(row.count);
         else ethnicityData.datasets[0].data[6] = ethnicityData.datasets[0].data[6] + parseInt(row.count);
       }
     });
@@ -188,11 +183,11 @@ class _Companies extends Component {
 
     const lineData = {
       labels: [
-        MONTHS[month-5 < 0 ? month+7 : month-5],
-        MONTHS[month-4 < 0 ? month+8 : month-4],
-        MONTHS[month-3 < 0 ? month+9 : month-3],
-        MONTHS[month-2 < 0 ? month+10 : month-2],
-        MONTHS[month-1 < 0 ? month+11 : month-1],
+        MONTHS[month - 5 < 0 ? month + 7 : month - 5],
+        MONTHS[month - 4 < 0 ? month + 8 : month - 4],
+        MONTHS[month - 3 < 0 ? month + 9 : month - 3],
+        MONTHS[month - 2 < 0 ? month + 10 : month - 2],
+        MONTHS[month - 1 < 0 ? month + 11 : month - 1],
         MONTHS[month]
       ],
       datasets: [
@@ -254,14 +249,14 @@ class _Companies extends Component {
       datasets: [{
         data: [300, 50, 100],
         backgroundColor: [
-        '#FF6384',
-        '#36A2EB',
-        '#FFCE56'
+          '#FF6384',
+          '#36A2EB',
+          '#FFCE56'
         ],
         hoverBackgroundColor: [
-        '#FF6384',
-        '#36A2EB',
-        '#FFCE56'
+          '#FF6384',
+          '#36A2EB',
+          '#FFCE56'
         ]
       }]
     };
@@ -276,14 +271,14 @@ class _Companies extends Component {
       datasets: [{
         data: [300, 50, 100],
         backgroundColor: [
-        '#FF6384',
-        '#36A2EB',
-        '#FFCE56'
+          '#FF6384',
+          '#36A2EB',
+          '#FFCE56'
         ],
         hoverBackgroundColor: [
-        '#FF6384',
-        '#36A2EB',
-        '#FFCE56'
+          '#FF6384',
+          '#36A2EB',
+          '#FFCE56'
         ]
       }]
     };
@@ -300,18 +295,18 @@ class _Companies extends Component {
             <div className="col-sm-7 col-md-9 flex-column-parent chart-area">
               <div className="row flex-child-1 flex-desktop">
                 <div className="col-md-6 flex-column-parent flex-child-1">
-                  <div className="chart-panel flex-child-1" style={{"marginBottom":"20px"}}>
-                    <Pie data={sentimentData}/>
+                  <div className="chart-panel flex-child-1" style={{"marginBottom": "20px"}}>
+                    <Pie data={this.getSentimentPieData()}/>
                   </div>
                 </div>
                 <div className="col-md-6 flex-column-parent flex-child-1">
-                  <div className="chart-panel flex-child-1" style={{"marginBottom":"20px"}}>
+                  <div className="chart-panel flex-child-1" style={{"marginBottom": "20px"}}>
                     <Line data={lineData}/>
                   </div>
                 </div>
               </div>
               <div className="row flex-child-1 flex-desktop">
-                <div className="col-md-6 flex-column-parent flex-child-1" style={{"marginBottom":"20px"}}>
+                <div className="col-md-6 flex-column-parent flex-child-1" style={{"marginBottom": "20px"}}>
                   <div className="chart-panel flex-child-1">
                     {!this.props.filters.ethnicity &&
                     <Pie data={this.getEthnicityPieData()}/>
@@ -321,7 +316,7 @@ class _Companies extends Component {
                     }
                   </div>
                 </div>
-                <div className="col-md-6 flex-column-parent flex-child-1" style={{"marginBottom":"20px"}}>
+                <div className="col-md-6 flex-column-parent flex-child-1" style={{"marginBottom": "20px"}}>
                   <div className="chart-panel flex-child-1">
                     {!this.props.filters.gender &&
                     <Pie data={this.getGenderPieData()}/>
@@ -348,13 +343,34 @@ class _Companies extends Component {
     });
   };
 
+  getDifferentSentiments = () => {
+    const {companies} = this.state;
+    const name = this.props.match.params.name;
+    const stats = {
+      positive: 0,
+      negative: 0,
+      mixed: 0,
+      neutral: 0,
+      totalCount: 0
+    };
+    let sentiment = 0;
+    companies[name].data.forEach((row) => {
+      const {positive, negative, mixed, neutral, count} = row;
+      stats.positive += positive;
+      stats.negative += negative;
+      stats.mixed += mixed;
+      stats.neutral += neutral;
+      stats.totalCount += count;
+    });
+    return stats;
+  };
   calculateSentiment = (month) => {
-    const { companies } = this.state;
+    const {companies} = this.state;
     const name = this.props.match.params.name;
     let sentiment = 0;
     companies[name].data.forEach((row) => {
-      const { positive, negative, mixed, neutral, count } = row;
-      const sum = (0.5 * parseFloat(neutral)) + parseFloat(positive) * 3 - parseFloat(negative )- 0.1 * parseFloat(mixed);
+      const {positive, negative, mixed, neutral, count} = row;
+      const sum = (0.5 * parseFloat(neutral)) + parseFloat(positive) * 3 - parseFloat(negative) - 0.1 * parseFloat(mixed);
 
       if (parseInt(row.time) - 1 === month) {
         sentiment += parseFloat(sum);
@@ -364,6 +380,7 @@ class _Companies extends Component {
     return 100 * (sentiment / companies[name].totalCount);
   }
 }
+
 
 const mapStateToProps = (state) => {
   return {
