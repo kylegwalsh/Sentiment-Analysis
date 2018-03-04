0693e71c6559369e5795a40181ec5b86096c7c44
Kyle Walsh <kylegwalsh@ufl.edu>
2018-03-04 02:04:27 -0500

    Finalized rankings table

--- client/src/pages/Rankings.js
+++ client/src/pages/Rankings.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import { Header } from '../components';
-import ReactTable from 'react-table';
-import 'react-table/react-table.css';
 import { connect } from "react-redux";
 import * as actions from "../actions";
+import BootstrapTable from 'react-bootstrap-table-next';
+import paginationFactory from 'react-bootstrap-table2-paginator';
 
 export class Rankings extends Component {
   constructor(props) {
@@ -19,43 +19,59 @@ export class Rankings extends Component {
       });
   }
   render() {
+    const products = [ {company:"Google",sentiment:80,diversity:80}, {company:"Apple",sentiment:40,diversity:20} ];
+    const columns = [{
+      dataField: 'company',
+      text: 'Company',
+      sort: true,
+      align: 'center'
+    }, {
+      dataField: 'sentiment',
+      text: 'Overall Sentiment',
+      sort: true,
+      align: 'center',
+      classes: (cell, row, rowIndex, colIndex) => {
+        if (products[rowIndex].sentiment > 0) return 'green-val';
+        else if(products[rowIndex].sentiment < 0) return 'red-val';
+        return '';
+      }
+    }, {
+      dataField: 'diversity',
+      text: 'Diversity Rating',
+      sort: true,
+      align: 'center'
+    }];
+    const defaultSorted = [{
+      dataField: 'sentiment',
+      order: 'desc'
+    }];
+    const rowEvents = {
+      onClick: (e, row, rowIndex) => {
+        this.props.history.push(`/companies/${products[rowIndex].company}`);
+      }
+    };
+    const rowClasses = 'hover-row';
+
     return (
       <div>
         <Header noFilter company={'Sentissimo'} />
-        <div>
 
+        <div className="text-center">
+          <h2><strong>Company Rankings</strong></h2>
         </div>
-        <ReactTable
-          getTdProps={(state, rowInfo, column, instance) => {
-            return {
-              onClick: () => {
-                this.props.history.push(`/companies/${rowInfo.original.name}`);
-              }
-            }
-          }}
-          data={this.state.companies}
-          columns={[
-            {
-              Header: 'Companies',
-              columns: [
-                {
-                  Header: 'Name',
-                  accessor: 'name'
-                },
-                {
-                  Header: 'Sentiment Score',
-                  accessor: 'sentiment'
-                },
-                {
-                  Header: 'Diversity Score',
-                  accessor: 'diversity'
-                }
-              ]
-            }
-          ]}
-          defaultPageSize={5}
-          className='-striped -highlight'
-        />
+
+        <div className="col-sm-8 col-sm-offset-2">
+          <BootstrapTable
+            keyField='company'
+            data={ products }
+            columns={ columns }
+            defaultSorted={ defaultSorted }
+            rowEvents={ rowEvents }
+            rowClasses={ rowClasses }
+            pagination={ paginationFactory() }
+            />
+        </div>
+
       </div>
     );
   }
